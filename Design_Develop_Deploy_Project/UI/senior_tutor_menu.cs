a0e15ce31f8cb1ffecabfaebacf813f9a85dd1c2
josefhardy <josefmhardy@gmail.com>
2025-10-31 16:48:41 +0000

    finished UI

--- Design_Develop_Deploy_Project/UI/senior_tutor_menu.cs
+++ Design_Develop_Deploy_Project/UI/senior_tutor_menu.cs
@@ -1,10 +1,73 @@
-﻿using System;
+﻿using Design.Develop.Deploy.Repos;
+using Design_Develop_Deploy_Project.Objects;
+using Design_Develop_Deploy_Project.Repos;
+using Design_Develop_Deploy_Project.Services;
+using Design_Develop_Deploy_Project.Utilities;
+using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Design_Develop_Deploy_Project.UI;
-
-public class SeniortutorMenu
+public class SeniorTutorMenu
 {
-	public SeniortutorMenu()
+    private readonly MeetingRepository meetingRepo;
+    private readonly StudentRepository studentRepo;
+    private readonly StatusRepository statusRepo;
+    private readonly SupervisorRepository supervisorRepo;
+    private readonly User user;
+    public SeniorTutorMenu(User user, string connectionString)
 	{
-	}
+        meetingRepo = new MeetingRepository(connectionString);
+        studentRepo = new StudentRepository(connectionString);
+        statusRepo = new StatusRepository(connectionString);
+        supervisorRepo = new SupervisorRepository(connectionString);
+        this.user = user;
+    }
+
+    public void ShowSeniorTutorMenu() 
+    {
+        bool exit = false;
+        while (!exit) {
+            var menuItems = new List<string>
+            {
+                "View all students",
+                "View all supervisors",
+                "View Supervisor performance",
+                "View at risk students",
+                "Logout"
+            };
+
+            Console.Clear();
+            Console.WriteLine($"Welcome {user.first_name}!");
+            int choice = ConsoleHelper.PromptForChoice(menuItems, "====== Senior Tutor Menu ======");
+
+            switch (choice)
+            {
+                case 1:
+                    tutorService.ViewAllStudents(user);
+                    ConsoleHelper.Pause();
+                    break;
+                case 2:
+                    tutorService.ViewAllSupervisors(user);
+                    ConsoleHelper.Pause();
+                    break;
+                case 3:
+                    tutorService.ViewSupervisorPerformance(user);
+                    ConsoleHelper.Pause();
+                    break;
+                case 4:
+                    tutorService.ViewAtRiskStudents(user);
+                    ConsoleHelper.Pause();
+                    break;
+                case 5:
+                    exit = true;
+                    break;
+                default:
+                    Console.WriteLine("Invaliud choice, please try again.");
+                    break;
+
+
+            } 
+        }
+    }
+
 }
--- Design_Develop_Deploy_Project/UI/supervisor_menu.cs
+++ Design_Develop_Deploy_Project/UI/supervisor_menu.cs
@@ -1,10 +1,111 @@
-﻿using System;
+﻿using System;                               
+using Design_Develop_Deploy_Project.Repos;
+using Design_Develop_Deploy_Project.Objects;
+using Design_Develop_Deploy_Project.Utilities;
+using Design_Develop_Deploy_Project.Services;
 
 namespace Design_Develop_Deploy_Project.UI;
 
 public class SupervisorMenu
 {
-	public SupervisorMenu()
+	private readonly SupervisorRepository supervisorRepo;
+	private readonly UserRepository userRepo;
+	private readonly Supervisor supervisor;
+	private readonly MeetingRepository meetingRepo;
+
+	public SupervisorMenu(User user, string connectionstring)
 	{
+		supervisorRepo = new SupervisorRepository(connectionstring);
+		userRepo = new UserRepository(connectionstring);
+		meetingRepo = new MeetingRepository(connectionstring);
+		supervisor = supervisorRepo.GetSupervisorByEmail(user.email);
 	}
+
+	public void ShowSupervisorMenu()
+	{
+		bool update_office_hours, update_well_being_check;
+
+        bool updateOfficeHours = false;
+        bool updateWellbeingCheck = false;
+        List<string> reminders = new();
+
+        if (supervisor.last_office_hours_update == null ||
+            (DateTime.UtcNow - supervisor.last_office_hours_update.Value).TotalDays > 7)
+        {
+            updateOfficeHours = true;
+            reminders.Add("your office hours");
+        }
+
+        if (supervisor.last_wellbeing_check == null ||
+            (DateTime.UtcNow - supervisor.last_wellbeing_check.Value).TotalDays > 7)
+        {
+            updateWellbeingCheck = true;
+            reminders.Add("your wellbeing check");
+        }
+
+        if (reminders.Count > 0)
+        {
+            string joined = string.Join(" and ", reminders);
+            Console.WriteLine($"{supervisor.first_name}, it has been more than a week since your last {joined}. Please update them soon.");
+        }
+
+        bool exit = false;
+        while (!exit) 
+        {
+            var menuItems = new List<string>
+            {
+                "View all your students",
+                "View details of specific student",
+                "View inactive students", 
+                "Book a meeting with a student",
+                "View your meetings",
+                "Update office hours",
+                "View your performance metrics",
+                "Logout"
+            };
+
+            Console.Clear();
+            Console.WriteLine($"Welcome {supervisor.first_name}!");
+            int choice = ConsoleHelper.PromptForChoice(menuItems, "====== Supervisor Menu ======");
+
+            switch(choice)
+            {
+                case 1:
+                    SupervisorService.ViewAllStudents(supervisor);
+                    ConsoleHelper.Pause();
+                    break;
+                case 2:
+                    SupervisorService.ViewSpecificStudent(supervisor);
+                    ConsoleHelper.Pause();
+                    break;
+                case 3:
+                    SupervisorService.ViewInactiveStudents(supervisor);
+                    ConsoleHelper.Pause();
+                    break;
+                case 4:
+                    SupervisorService.BookMeetingWithStudent(supervisor);
+                    ConsoleHelper.Pause();
+                    break;
+                case 5:
+                    SupervisorService.ViewMeetings(supervisor);
+                    ConsoleHelper.Pause();
+                    break;
+                case 6:
+                    SupervisorService.UpdateOfficeHours(supervisor);
+                    ConsoleHelper.Pause();
+                    break;
+                case 7:
+                    SupervisorService.ViewPerformanceMetrics(supervisor);
+                    ConsoleHelper.Pause();
+                    break;
+                case 8:
+                    exit = true;
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice, please try again");
+                    break;
+            }
+        }
+
+    }
 }
